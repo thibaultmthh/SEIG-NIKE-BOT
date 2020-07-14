1da96055bf9983713f1a074b234881367619c9f7
Michel Farnier <tanmath14@gmail.com>
2020-07-14 09:38:30 +0200

    test

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,11 +1,13 @@
 Tasks = document.getElementById("Tasks")
 Shipping = document.getElementById("Shipping")
 Accounts = document.getElementById("Accounts")
+Tasks_real = document.getElementById("Tasks_real")
 
 
 Tasks_div = document.getElementById("Tasks_div")
 profils_div = document.getElementById("profils_div")
 Accounts_div = document.getElementById("Accounts_div")
+Tasks_real_div =document.getElementById("Tasks_real_div")
 
 
 actuel_ligne = Tasks
@@ -35,6 +37,15 @@ Accounts.addEventListener("click", function() {
   Accounts_div.style.display = "block"
   actuel_ligne = Accounts;
   actuel_main = Accounts_div
+})
+
+Tasks_real.addEventListener("click", function() {
+  actuel_ligne.classList.remove("ligne_bas")
+  Tasks_real.classList.add("ligne_bas")
+  actuel_main.style.display = "none"
+  Tasks_real_div.style.display = "block"
+  actuel_ligne = Tasks_real;
+  actuel_main = Tasks_real_div
 })
 
 var modal = document.getElementById("myModal");
